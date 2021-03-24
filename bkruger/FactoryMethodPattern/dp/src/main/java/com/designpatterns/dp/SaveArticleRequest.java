84ec2ac4a610b714a38af7bfb4575b53c1b3d4b3
Byron Kruger <bkruger@jhb.dvt.co.za>
2021-03-24 11:17:49 +0200

    Completed factory method pattern.

---
+++ bkruger/FactoryMethodPattern/dp/src/main/java/com/designpatterns/dp/SaveArticleRequest.java
@@ -0,0 +1,19 @@
+package com.designpatterns.dp;
+
+public class SaveArticleRequest implements Request{
+    private String articleTitle;
+    private String articleContent;
+
+    public SaveArticleRequest(String articleTitle, String articleContent){
+        this.articleTitle = articleTitle;
+        this.articleContent = articleContent;
+    }
+
+    public String getArticleTitle(){
+        return this.articleTitle;
+    }
+
+    public String getArticleContent(){
+        return this.articleContent;
+    }
+}
