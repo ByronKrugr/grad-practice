4bf9a127ea90ebd8229afdc7a95c2b69dfa77a9f
Byron Kruger <bkruger@jhb.dvt.co.za>
2021-04-04 13:09:20 +0200

    Migrating from outdated branch.

--- bkruger/BlogWebAppMultitier/blog/src/main/java/com/blogwebapp/bwa/controllers/readArticle/ArticleReadController.java
+++ bkruger/BlogWebAppMultitier/blog/src/main/java/com/blogwebapp/bwa/controllers/readArticle/ArticleReadController.java
@@ -1,7 +1,12 @@
 package com.blogwebapp.bwa.controllers.readArticle;
 
+import com.blogwebapp.bwa.RequestBuilderImpl;
+import com.blogwebapp.bwa.UseCaseFactoryImpl;
 import com.blogwebapp.bwa.repositories.Article;
 import com.blogwebapp.bwa.ArticleService;
+import com.blogwebapp.bwa.services.Request;
+import com.blogwebapp.bwa.services.Response;
+import com.blogwebapp.bwa.services.UseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,15 +14,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.HashMap;
+
 @Controller
 public class ArticleReadController {
+    private UseCaseFactoryImpl useCaseFactory;
+    private RequestBuilderImpl requestBuilder;
+    private UseCase readArticleUseCase;
 
-    @Autowired
-    private ArticleService articleService;
+    public ArticleReadController(){
+        this.useCaseFactory = new UseCaseFactoryImpl();
+        this.requestBuilder = new RequestBuilderImpl();
+    }
 
     @GetMapping("/read/{id}")
-    public ResponseEntity<Article> readArticle(@PathVariable String id){
-        Article article = articleService.getArticle(id);
-        return new ResponseEntity<>(article, HttpStatus.FOUND);
+    public ResponseEntity<Response> readArticle(@PathVariable String id){
+        this.readArticleUseCase = useCaseFactory.makeUseCase("READ_ARTICLE");
+        Request readArticleRequest = requestBuilder.buildRequest("READ_ARTICLE",
+                new HashMap<>(){{ put("id", id); }});
+        this.readArticleUseCase = useCaseFactory.makeUseCase("READ_ARTICLE");
+        Response readArticleResponse = this.readArticleUseCase.execute(readArticleRequest);
+        return new ResponseEntity<>(readArticleResponse, HttpStatus.FOUND);
     }
 }
--- bkruger/BlogWebAppMultitier/blog/src/main/java/com/blogwebapp/bwa/controllers/saveArticle/ArticleSaveController.java
+++ bkruger/BlogWebAppMultitier/blog/src/main/java/com/blogwebapp/bwa/controllers/saveArticle/ArticleSaveController.java
@@ -1,7 +1,12 @@
 package com.blogwebapp.bwa.controllers.saveArticle;
 
+import com.blogwebapp.bwa.RequestBuilderImpl;
+import com.blogwebapp.bwa.UseCaseFactoryImpl;
 import com.blogwebapp.bwa.repositories.Article;
 import com.blogwebapp.bwa.ArticleService;
+import com.blogwebapp.bwa.services.Request;
+import com.blogwebapp.bwa.services.Response;
+import com.blogwebapp.bwa.services.UseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,15 +14,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.HashMap;
+
 @Controller
 public class ArticleSaveController {
+    private UseCaseFactoryImpl useCaseFactory;
+    private RequestBuilderImpl requestBuilder;
+    private UseCase saveArticleUseCase;
 
-    @Autowired
-    private ArticleService articleService;
+    public ArticleSaveController(){
+        this.useCaseFactory = new UseCaseFactoryImpl();
+        this.requestBuilder = new RequestBuilderImpl();
+        this.saveArticleUseCase = this.useCaseFactory.makeUseCase("SAVE_ARTICLE");
+    }
 
     @PostMapping("/save")
-    public ResponseEntity<Article> saveArticle(@RequestBody Article article){
-        Article savedArticle = articleService.saveArticle(article);
-        return new ResponseEntity<>(savedArticle, HttpStatus.CREATED);
+    public ResponseEntity<Response> saveArticle(@RequestBody Article article){
+        Request saveArticleRequest = requestBuilder.buildRequest("SAVE_ARTICLE",
+                new HashMap<>(){{ put("title", article.getTitle());
+                    put("tldr", article.getTldr());
+                    put("content", article.getContent()); }});
+        Response saveArticleResponse = this.saveArticleUseCase.execute(saveArticleRequest);
+        return new ResponseEntity<>(saveArticleResponse, HttpStatus.CREATED);
     }
 }
