4cd466c89da145879a33dcb58706c01228cd0915
Byron Kruger <bkruger@jhb.dvt.co.za>
2021-04-07 11:02:59 +0200

    Initial commit.

---
+++ bkruger/PMDPluginIn/ppu/src/main/java/za/co/dvt/ppu/PmdPluginUsageApplication.java
@@ -0,0 +1,13 @@
+package za.co.dvt.ppu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmdPluginUsageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmdPluginUsageApplication.class, args);
+	}
+
+}
---
+++ bkruger/PMDPluginIn/ppu/src/test/java/za/co/dvt/ppu/PmdPluginUsageApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.dvt.ppu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmdPluginUsageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
