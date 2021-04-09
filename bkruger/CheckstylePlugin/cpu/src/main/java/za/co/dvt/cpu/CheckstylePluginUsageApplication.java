3e499142026408c8f4324fbe38218f4f377b4792
Byron Kruger <bkruger@jhb.dvt.co.za>
2021-04-09 18:00:11 +0200

    Initial commit and bkruger-directory wide .gitignore update.

---
+++ bkruger/CheckstylePlugin/cpu/src/main/java/za/co/dvt/cpu/CheckstylePluginUsageApplication.java
@@ -0,0 +1,13 @@
+package za.co.dvt.cpu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckstylePluginUsageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckstylePluginUsageApplication.class, args);
+	}
+
+}
