266736e4688dd28dc8c0825e7ce84cd546699234
Byron Kruger <bkruger@jhb.dvt.co.za>
2021-04-16 14:56:39 +0200

    Refactored tests.

--- bkruger/BlogWebAppMultitier/blog/src/test/java/com/blogwebapp/bwa/ArticleBuilderUnitTest.java
+++ bkruger/BlogWebAppMultitier/blog/src/test/java/com/blogwebapp/bwa/ArticleBuilderUnitTest.java
@@ -3,6 +3,7 @@ package com.blogwebapp.bwa;
 import com.blogwebapp.bwa.repositories.Article;
 import com.blogwebapp.bwa.repositories.ArticleBuilder;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -27,17 +28,21 @@ public class ArticleBuilderUnitTest {
 //        ArticleBuilder articleBuilder = new ArticleBuilder();
 //        Article article = new Article(articleBuilder);
 //    }
+    private ArticleBuilder articleBuilder;
+
+    @BeforeEach
+    public void setUp(){
+        this.articleBuilder = new ArticleBuilder();
+    }
 
     @Test
     public void canbBuildArticleWithId(){
-        ArticleBuilder articleBuilder = new ArticleBuilder();
         Article article = articleBuilder.withId("id").build();
         assertThat(article.getId()).isEqualTo("id");
     }
 
     @Test
     public void canBuildArticleWithTitle(){
-        ArticleBuilder articleBuilder = new ArticleBuilder();
         Article article = articleBuilder.withId("id").withTitle("title").build();
         assertThat(article.getId()).isEqualTo("id");
         assertThat(article.getTitle()).isEqualTo("title");
