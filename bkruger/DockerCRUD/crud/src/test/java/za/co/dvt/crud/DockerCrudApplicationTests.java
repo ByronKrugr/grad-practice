3f480e9dac7d69bf8ef64ac1b0d31747317ce8c4
Byron Kruger <bkruger@jhb.dvt.co.za>
2021-04-28 12:56:38 +0200

    Initial commit.

---
+++ bkruger/DockerCRUD/crud/src/test/java/za/co/dvt/crud/DockerCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.dvt.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
